2b30dcc215fc105e34fad26b5f32357dadcb60de
tuandomvn <tuandomvn@gmail.com>
2026-03-08 22:07:21 +0700

    init

---
+++ WebApplication1/WebApplication1/Models/ClinicModels.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace WebApplication1.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public DateTime Birth { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class Visit
+    {
+        public int AppointmentId { get; set; }
+        public DateTime Date { get; set; }
+        public string Doctor { get; set; }
+        public string Summary { get; set; }
+    }
+
+    public class Appointment
+    {
+        public int AppointmentId { get; set; }
+        public DateTime Date { get; set; }
+        public string Doctor { get; set; }
+        public string Type { get; set; }
+    }
+}
